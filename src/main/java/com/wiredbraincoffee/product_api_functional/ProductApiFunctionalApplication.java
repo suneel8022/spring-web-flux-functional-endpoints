ceff281c41f2eba5f2390cd38c82d71a2e309adb
Suneel <suneelhere8022@gmail.com>
2025-01-19 20:22:12 +0530

    Setting up with Spring Initlzr

---
+++ src/main/java/com/wiredbraincoffee/product_api_functional/ProductApiFunctionalApplication.java
@@ -0,0 +1,13 @@
+package com.wiredbraincoffee.product_api_functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiFunctionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiredbraincoffee/product_api_functional/ProductApiFunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiredbraincoffee.product_api_functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiFunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
