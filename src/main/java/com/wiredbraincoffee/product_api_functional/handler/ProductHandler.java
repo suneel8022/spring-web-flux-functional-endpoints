943c6f5cc660ecb72f314b05e8e76fd0bf701a74
Suneel <suneelhere8022@gmail.com>
2025-01-19 21:51:37 +0530

    Added GET method for products using Functional Endpoint

--- src/main/java/com/wiredbraincoffee/product_api_functional/handler/ProductHandler.java
+++ src/main/java/com/wiredbraincoffee/product_api_functional/handler/ProductHandler.java
@@ -1,8 +1,17 @@
 package com.wiredbraincoffee.product_api_functional.handler;
 
 
+import com.wiredbraincoffee.product_api_functional.model.Product;
 import com.wiredbraincoffee.product_api_functional.repository.ProductRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
 @Component
 public class ProductHandler {
@@ -12,4 +21,33 @@ public class ProductHandler {
     public ProductHandler(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+
+                    // get All Products
+    public Mono<ServerResponse> getAllProducts(ServerRequest serverRequest){
+        Flux<Product> products = productRepository.findAll();
+
+        return ServerResponse.ok()
+                .contentType(APPLICATION_JSON)
+                .body(products,Product.class);
+    }
+
+                    // get a single product
+    public Mono<ServerResponse> getProduct(ServerRequest serverRequest){
+        String id = serverRequest.pathVariable("id");
+
+        Mono<Product> productMono = this.productRepository.findById(id);
+        Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+
+        return productMono
+                .flatMap(product ->
+                        ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(fromValue(product))
+                )
+                .switchIfEmpty(notFound);
+
+    }
+
+
 }
